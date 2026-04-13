83893e59b029dbd140e7c39b3bcb1ef8080e8742
Thomas <77514919+TB516@users.noreply.github.com>
2026-04-13 18:59:16 -0400

    Updated sha key

--- Casks/helium-linux.rb
+++ Casks/helium-linux.rb
@@ -4,8 +4,8 @@ cask "helium-linux" do
   os linux: "linux"
 
   version "0.11.1.1"
-  sha256 arm: "ca3dfa418003ac7853a1aaeead2fa1033800fa0322b7d3d248c9353b956f1f7c",
-         intel: "d72ea775519ea9aeb52a9991bfd76e91f7e90b176b3b2f7b8cc790732981d1b7"
+  sha256 arm64_linux: "ca3dfa418003ac7853a1aaeead2fa1033800fa0322b7d3d248c9353b956f1f7c",
+         intel_linux: "d72ea775519ea9aeb52a9991bfd76e91f7e90b176b3b2f7b8cc790732981d1b7"
 
   url "https://github.com/imputnet/helium-linux/releases/download/#{version}/helium-#{version}-#{arch}_linux.tar.xz"
   name "Helium"
